f1924b759597856c8b83e4c99e995e6abd28ba9e
Zero King <l2dy@macports.org>
2018-03-11 13:57:50 +0000

    pr: List of members is paginated

--- pr/githubapi/org.go
+++ pr/githubapi/org.go
@@ -3,6 +3,18 @@ package githubapi
 import "github.com/google/go-github/github"
 
 func (client *githubClient) ListOrgMembers(org string) ([]*github.User, error) {
-	users, _, err := client.Organizations.ListMembers(client.ctx, org, nil)
-	return users, err
+	var allMembers []*github.User
+	opt := &github.ListMembersOptions{ListOptions: github.ListOptions{PerPage: 30}}
+	for {
+		users, resp, err := client.Organizations.ListMembers(client.ctx, org, opt)
+		if err != nil {
+			return nil, err
+		}
+		allMembers = append(allMembers, users...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	return allMembers, nil
 }
