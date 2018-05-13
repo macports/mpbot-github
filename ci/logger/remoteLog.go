7fae78f18a3e9fd6b98e3d4c0389d581f13398c6
Zero King <l2dy@macports.org>
2018-05-13 04:42:09 +0000

    ci: use retryable HTTP client

--- ci/logger/remoteLog.go
+++ ci/logger/remoteLog.go
@@ -2,11 +2,14 @@ package logger
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 var pasteURL = &url.URL{
@@ -19,83 +22,88 @@ type remoteLogger struct {
 	logBigFileChan chan *LogFile
 	parent         *Logger
 	quitChan       chan byte
-	httpClient     *http.Client
+	httpClient     *retryablehttp.Client
 }
 
 func newRemoteLogger(parent *Logger) *remoteLogger {
-	return &remoteLogger{
+	r := &remoteLogger{
 		logBigFileChan: make(chan *LogFile, 4),
 		parent:         parent,
 		quitChan:       make(chan byte),
-		httpClient: &http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			},
-		},
+		httpClient:     retryablehttp.NewClient(),
 	}
+	r.httpClient.Logger = nil
+	r.httpClient.HTTPClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	return r
 }
 
 func (r *remoteLogger) run() {
 	for {
+		var err error
+		var fieldName string
+
 		select {
 		case logBigFile := <-r.logBigFileChan:
 			if logBigFile == nil {
 				r.parent.LogChan <- &LogText{FieldName: "", Text: nil}
 				return
 			}
-			fileInfo, err := os.Stat(logBigFile.Filename)
-			if err == nil {
-				for i := 1; i < 3; i++ {
-					file, iErr := os.Open(logBigFile.Filename)
-					if iErr != nil {
-						err = iErr
-						break
-					}
-					buf := new(bytes.Buffer)
-					mimeWriter := multipart.NewWriter(buf)
-					writer, iErr := mimeWriter.CreateFormField("paste")
-					if iErr != nil {
-						err = iErr
-						break
-					}
-					// Max 8 MiB
-					const trimNote = "*log trimmed*\n"
-					if fileSize := fileInfo.Size(); fileSize > 8*1024*1024-int64(len(trimNote)) {
-						file.Seek(fileSize-8*1024*1024+int64(len(trimNote)), 0)
-						io.WriteString(writer, trimNote)
-					}
-					io.Copy(writer, file)
-					file.Close()
-					mimeWriter.Close()
-					resp, iErr := r.httpClient.Post(pasteURL.String(), mimeWriter.FormDataContentType(), buf)
-					if iErr != nil {
-						err = iErr
-						continue
-					}
-					resp.Body.Close()
-					loc := resp.Header.Get("Location")
-					if loc == "" {
-						err = iErr
-						continue
-					}
-					u, iErr := pasteURL.Parse(loc)
-					if iErr != nil {
-						err = iErr
-						break
-					}
-					r.parent.LogChan <- &LogText{
-						logBigFile.FieldName + "-pastebin",
-						[]byte(u.String()),
-					}
-					err = nil
-					break
-				}
+			fieldName = logBigFile.FieldName
+			fileInfo, iErr := os.Stat(logBigFile.Filename)
+			if iErr != nil {
+				err = iErr
+				break
 			}
-			if err != nil {
-				r.parent.LogChan <- &LogText{
-					logBigFile.FieldName + "-pastebin-fail",
-					[]byte(err.Error()),
-				}
+			file, iErr := os.Open(logBigFile.Filename)
+			if iErr != nil {
+				err = iErr
+				break
+			}
+			buf := new(bytes.Buffer)
+			mimeWriter := multipart.NewWriter(buf)
+			writer, iErr := mimeWriter.CreateFormField("paste")
+			if iErr != nil {
+				err = iErr
+				break
+			}
+			// Max 8 MiB
+			const trimNote = "*log trimmed*\n"
+			if fileSize := fileInfo.Size(); fileSize > 8*1024*1024-int64(len(trimNote)) {
+				file.Seek(fileSize-8*1024*1024+int64(len(trimNote)), 0)
+				io.WriteString(writer, trimNote)
+			}
+			io.Copy(writer, file)
+			file.Close()
+			mimeWriter.Close()
+			postForm := bytes.NewReader(buf.Bytes())
+			resp, iErr := r.httpClient.Post(pasteURL.String(), mimeWriter.FormDataContentType(), postForm)
+			if iErr != nil {
+				err = iErr
+				break
+			}
+			resp.Body.Close()
+			loc := resp.Header.Get("Location")
+			if loc == "" {
+				err = errors.New("missing Location header")
+				break
+			}
+			u, iErr := pasteURL.Parse(loc)
+			if iErr != nil {
+				err = iErr
+				break
+			}
+			r.parent.LogChan <- &LogText{
+				fieldName + "-pastebin",
+				[]byte(u.String()),
+			}
+		}
+
+		if err != nil {
+			r.parent.LogChan <- &LogText{
+				fieldName + "-pastebin-fail",
+				[]byte(err.Error()),
 			}
 		}
 	}
